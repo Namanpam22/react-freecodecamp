b52170842f98c0e79ab1d58a9b273aed0fa91e51
Namanpam22 <namanpam22@gmail.com>
2021-10-30 13:05:25 +0530

    accessing data using array

--- src/index.js
+++ src/index.js
@@ -3,74 +3,29 @@ import ReactDOM from "react-dom";
 // css
 import "./index.css";
 //setup vars
-const firstbook = {
-  author: "Morgan Housel",
-  title: "The psychology of money",
-  img: "https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg",
-};
-const secondbook = {
-  author: "Robert kiyosaki",
-  title: "Rich Dad,Poor Dad",
-  img: "https://images-eu.ssl-images-amazon.com/images/I/81dQwQlmAXL._AC_UL200_SR200,200_.jpg",
-};
-const thirdbook = {
-  author: "Chetan Bhagat",
-  title: "400 Days",
-  img: "https://images-eu.ssl-images-amazon.com/images/I/81tSFxicufL._AC_UL200_SR200,200_.jpg",
-};
+const books = [
+  {
+    author: "Morgan Housel",
+    title: "The psychology of money",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    author: "Robert kiyosaki",
+    title: "Rich Dad,Poor Dad",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81dQwQlmAXL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    author: "Chetan Bhagat",
+    title: "400 Days",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81tSFxicufL._AC_UL200_SR200,200_.jpg",
+  },
+];
 function BookList() {
-  return (
-    <section className="bookrow">
-      <Book
-        img={firstbook.img}
-        title={firstbook.title}
-        author={firstbook.author}
-      />
-      <Book
-        img={secondbook.img}
-        title={secondbook.title}
-        author={secondbook.author}
-      />
-      <Book
-        img={thirdbook.img}
-        title={thirdbook.title}
-        author={thirdbook.author}
-        number={22}
-      />
-      <Book
-        img={firstbook.img}
-        title={firstbook.title}
-        author={firstbook.author}
-      />
-      <Book
-        img={secondbook.img}
-        title={secondbook.title}
-        author={secondbook.author}
-      />
-      <Book
-        img={thirdbook.img}
-        title={thirdbook.title}
-        author={thirdbook.author}
-        number={22}
-      />
-      <Book
-        img={firstbook.img}
-        title={firstbook.title}
-        author={firstbook.author}
-      />
-      <Book
-        img={secondbook.img}
-        title={secondbook.title}
-        author={secondbook.author}
-      />
-      <Book
-        img={thirdbook.img}
-        title={thirdbook.title}
-        author={thirdbook.author}
-        number={22}
-      />
-    </section>
-  );
+  return <section className="bookrow">{books.map((book) => {
+    const{img,title,author}=book;
+    return <Book></Book>
+  })};
+  </section>;
 }
 
 const Book = ({ img, title, author }) => {
